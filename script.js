e8f794a5a4bc771d9facbe4adecdba7e882b1e28
JOD <tj1998g@gmail.com>
2020-12-10 02:52:17 +0500

    fix
    fix

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 "use strict";
 
 let ulElem = document.querySelector('.ul');
-let liElem = document.querySelector('li');
+let liElem = document.querySelectorAll('li');
 let inputElem = document.querySelector('.input');
 let buttonElem = document.querySelector('.button');
 
-let inputElemBlok = () => {
+let buttonElemBlok = () => {
 	buttonElem.disabled = true;
 };
 
@@ -14,13 +14,18 @@ inputElem.addEventListener('input', () => {
 		buttonElem.disabled = false;
 	}
 });
+
 buttonElem.addEventListener('click', (item) => {
 	let li = document.createElement('li');
 	item = inputElem.value;
-	console.log('li: ', li);
-	ulElem.append(li) ;
-	inputElem.value = '';
-	inputElemBlok();
-} );
+	li.innerHTML = `<li>${item}</li>`;
 
-inputElemBlok();
+	// ulElem.append(liValue) ;
+
+	inputElem.value = '';
+
+	buttonElemBlok();
+
+});
+
+buttonElemBlok();
