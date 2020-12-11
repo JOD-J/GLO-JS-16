33bb035cd365e9988279a84bc846fa9c0426486c
JOD <tj1998g@gmail.com>
2020-12-11 20:51:30 +0500

    fix
    fix

--- script.js
+++ script.js
@@ -1,31 +1,137 @@
 "use strict";
 
-let ulElem = document.querySelector('.ul');
-let liElem = document.querySelectorAll('li');
-let inputElem = document.querySelector('.input');
-let buttonElem = document.querySelector('.button');
-
-let buttonElemBlok = () => {
-	buttonElem.disabled = true;
+const calculator = {
+	sum: function(){
+	  // ваш код
+	},
+	mult: function(){
+	  // ваш код
+	},
+	show: function(){
+	  // ваш код
+	}
 };
 
-inputElem.addEventListener('input', () => {
-	if (inputElem.value !== ''){
-		buttonElem.disabled = false;
-	}
-});
 
-buttonElem.addEventListener('click', (item) => {
-	let li = document.createElement('li');
-	item = inputElem.value;
-	li.innerHTML = `<li>${item}</li>`;
 
-	// ulElem.append(liValue) ;
 
-	inputElem.value = '';
 
-	buttonElemBlok();
 
-});
 
-buttonElemBlok();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const myLesson = [
+// 	{lesson: 1, type: 'basic', points: 2},
+// 	{lesson: 2, type: 'additional', points: 4},
+// 	{lesson: 3, type: 'basic', points: 6},
+// 	{lesson: 4, type: 'additional', points: 3},
+// 	{lesson: 5, type: 'basic', points: 4},
+// 	{lesson: 6, type: 'basic', points: 2},
+// 	{lesson: 7, type: 'additional', points: 2},
+// 	{lesson: 8, type: 'basic', points: 6},
+// 	{lesson: 9, type: 'basic', points: 4},
+// 	{lesson: 10, type: 'basic', points: 6},
+// 	{lesson: 11, type: 'additional', points: 5}, 
+// 	{lesson: 12, type: 'basic', points: 2}, 
+// 	{lesson: 13, type: 'additional', points: 2}, 
+// 	{lesson: 14, type: 'basic', points: 4},
+// 	{lesson: 15, type: 'additional', points: 1},
+// 	{lesson: 16, type: 'additional', points: 7},
+// 	];
+
+// 	for (let i = 0; i < myLesson.length; i++) {
+// 		if (myLesson[i].type === 'basic') {
+// 			myLesson[i].points /= 2;
+// 		} else {
+// 			myLesson.splice(i, 1);
+// 			i--;
+// 		}
+// 	}
+	
+// 	console.log(myLesson);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let ulElem = document.querySelector('.ul');
+// let liElem = document.querySelectorAll('li');
+// let inputElem = document.querySelector('.input');
+// let buttonElem = document.querySelector('.button');
+
+// let buttonElemBlok = () => {
+// 	buttonElem.disabled = true;
+// };
+
+// inputElem.addEventListener('input', () => {
+// 	if (inputElem.value !== ''){
+// 		buttonElem.disabled = false;
+// 	}
+// });
+
+// buttonElem.addEventListener('click', (item) => {
+// 	let li = document.createElement('li');
+// 	item = inputElem.value;
+// 	li.innerHTML = `<li>${item}</li>`;
+
+// 	// ulElem.append(liValue) ;
+
+// 	inputElem.value = '';
+
+// 	buttonElemBlok();
+
+// });
+
+// buttonElemBlok();
