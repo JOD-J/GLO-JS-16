fb06b28f04f8b8a8d3c00f95358ca471b24db6e8
JOD <tj1998g@gmail.com>
2020-12-15 00:24:11 +0500

    fix
    fix

---
+++ dayTime/js/script.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+"use strict";
+
+const divElem = document.querySelector('.div'),
+	date = new Date();
+
+const showDate = () => {
+	divElem.style.width = '500px';
+	divElem.style.height = '200px';
+	divElem.style.border = '5px solid red';
+
+
+};
+
+showDate();
+
--- js/script.js
+++ js/script.js
@@ -3,45 +3,53 @@
 // eslint-disable-next-line strict
 window.addEventListener('DOMContentLoaded', ()  => {
 	'use sctrict';
-//======================================================countTimer===========================================================
+	//======================================================countTimer===========================================================
 	// Timer
 	function countTimer(deadline) {
-		const timerHoursElem = document.querySelector('#timer-hours'),
-			timerMinutesElem = document.querySelector('#timer-minutes'),
-			timerSecondsElem = document.querySelector('#timer-seconds');
-//======================================================getTimeRemaining======================================================
+		const timerHoursElem = document.querySelector('#timer-hours'),		// элементы со страницы (hours).
+			timerMinutesElem = document.querySelector('#timer-minutes'),	// элементы со страницы (minutes).
+			timerSecondsElem = document.querySelector('#timer-seconds');	// элементы со страницы (seconds).
+		//======================================================getTimeRemaining======================================================
 		function getTimeRemaining() {
 			const	dateStop = new Date(deadline).getTime(),	// экземпляр класса date так мы получим конечную дату через метод getTime находим милисекунды.
 				dateNow = new Date().getTime(),					// получаем текущю дату через метод getTime находим милисекунды.
 				timeRemaining = (dateStop - dateNow) / 1000,	// разница между двумя датами |1000 получаем секунды.
 				seconds = Math.floor(timeRemaining % 60),		// % 60 остаток от деления.
 				minutes = Math.floor((timeRemaining / 60) % 60),
-				hours = Math.floor((timeRemaining / 60 / 60) % 24);
+				hours = Math.floor(timeRemaining / 60 / 60);
 			return { timeRemaining, hours, minutes, seconds };
 		}
-		
-//===============================================\\\\\\\getTimeRemaining======================================================
-//======================================================updateClock===========================================================
+		//===============================================\\\\\\\getTimeRemaining======================================================
+		//======================================================updateClock===========================================================
+		const  deletInterval = setInterval(updateClock, 1000); 	// setInterval
 		function updateClock() {
 			const timer = getTimeRemaining();
 			if (timer.timeRemaining >= 0) {
 				timerHoursElem.textContent = timer.hours;			// выводим hours в html.
 				timerMinutesElem.textContent = timer.minutes;		// выводим minutes в html.
 				timerSecondsElem.textContent = timer.seconds;		// выводим seconds в html.
-				console.log('if', timer.timeRemaining);
+				console.log('if', timer.timeRemaining);				// проверка работы setInterval.
+				if (timer.hours < 10) {
+					timerHoursElem.textContent = '0' +  timer.hours;		// конкантенация строки с числом hours.
+				}
+				if (timer.minutes < 10) {
+					timerMinutesElem.textContent = '0' +   timer.minutes;		// конкантенация строки с числом minutes.
+				}
+				if (timer.seconds < 10) {
+					timerSecondsElem.textContent = '0' +   timer.seconds;		// конкантенация строки с числом seconds.
+				}
 			} else {
-				clearInterval(updateClockInterval);
-				timerHoursElem.textContent = '00';		// остановка часов при достижении цели.
-				timerMinutesElem.textContent = '00';	// остановка часов при достижении цели.
-				timerSecondsElem.textContent = '00';	// остановка часов при достижении цели.
-				console.log('else', timer.timeRemaining);
+				clearInterval(deletInterval);				// удаление deletInterval (удаление setInterval).
+				timerHoursElem.textContent = '00';			// присвоение значение 00 для hours в сулчае окончание акции.
+				timerMinutesElem.textContent = '00';		// присвоение значение 00 для minutes в сулчае окончание акции.
+				timerSecondsElem.textContent = '00';		// присвоение значение 00 для seconds в сулчае окончание акции.
+				console.log('else', timer.timeRemaining);	// проверка удаление setInterval.
 
 			}
 		}
-//==============================================\\\\\\\updateClock===========================================================
-		const  updateClockInterval = setInterval(updateClock, 1000);
-}
-//==============================================\\\\\\\countTimer===========================================================
-	countTimer('15 December 2020');
+		//==============================================\\\\\\\updateClock===========================================================
+	}
+	//==============================================\\\\\\\countTimer===========================================================
+	countTimer('30 December 2020 ');
 });
 //==============================================\\\\\\\DOMContentLoaded======================================================
