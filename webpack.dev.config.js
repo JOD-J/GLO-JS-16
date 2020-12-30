456733fe4cf951989a33ed132f939f24cd41614c
JOD <tj1998g@gmail.com>
2020-12-31 01:39:13 +0500

    fix
    fix

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,6 +2,11 @@ const path = require('path');
 
 module.exports = {
 	entry: './src/index.js',
+	devServer: {
+		open: true,
+		port: 8085,
+		hot: true,
+	},
 	output: {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, './dist')
